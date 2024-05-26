1e189f5cf5c1100d364a90cbb9998ad36962a6c9
95ayman <aymandzfr@gmail.com>
2024-05-26 23:00:28 +0200

    Delete robot.h

--- robot.h
+++
@@ -1,9 +0,0 @@
-#ifndef ROBOT_H
-#define ROBOT_H
-
-#include "grille.h"
-
-// Déplacer le robot selon l'entrée de l'utilisateur
-int deplacerRobot(char grille[][MAX_LARGEUR], int hauteur, int largeur, int startX, int startY, int maxDeplacements);
-
-#endif // ROBOT_H
